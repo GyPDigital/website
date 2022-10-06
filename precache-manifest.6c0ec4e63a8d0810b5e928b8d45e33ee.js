2139be464ebd2cdf9d0a90462985096c4e403156
GyPDigital <gypinformaticaytecnologia@gmail.com>
2022-10-06 03:04:19 -0500

    Updates

---
+++ precache-manifest.6c0ec4e63a8d0810b5e928b8d45e33ee.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "19c1a53fdee3a0547164",
+    "url": "/website/static/css/main.93976503.chunk.css"
+  },
+  {
+    "revision": "19c1a53fdee3a0547164",
+    "url": "/website/static/js/main.19c1a53f.chunk.js"
+  },
+  {
+    "revision": "4d02947106305e1cc751",
+    "url": "/website/static/js/1.4d029471.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "0c5493380928275b0e8e5c3743045657",
+    "url": "/website/static/media/carbon.0c549338.jpg"
+  },
+  {
+    "revision": "76547b41794eb2d04b00fd5e05515113",
+    "url": "/website/static/media/accesorios.76547b41.jpg"
+  },
+  {
+    "revision": "b8b535c2b487f08bda5f62bea8dba436",
+    "url": "/website/static/media/Poppins-Bold-700.b8b535c2.woff"
+  },
+  {
+    "revision": "e4d2da94cf6853dc79db00fc547b0972",
+    "url": "/website/static/media/Avenir-light.e4d2da94.woff"
+  },
+  {
+    "revision": "834ba03cbefa7f604c440048f87de3eb",
+    "url": "/website/static/media/Peaches-and-Cream-Regular.834ba03c.woff"
+  },
+  {
+    "revision": "74cfde7d4ab6efbc1f7b2397517fadfc",
+    "url": "/website/static/media/fondo-gyp-lc.74cfde7d.jpg"
+  },
+  {
+    "revision": "88eef1f5c6524eb867a13d540b82a3fd",
+    "url": "/website/static/media/banner-gyp-largo.88eef1f5.jpg"
+  },
+  {
+    "revision": "047494c63b682e15ff6dec5abb813fab",
+    "url": "/website/static/media/audifonos.047494c6.jpg"
+  },
+  {
+    "revision": "6f047b0f87e3b51009651ac74d0857c1",
+    "url": "/website/static/media/cable-usb-tipo-b-fly.6f047b0f.jpg"
+  },
+  {
+    "revision": "4de2919edf36cefcfae9fd33a2618797",
+    "url": "/website/static/media/cable-usb-tipo-b.4de2919e.jpg"
+  },
+  {
+    "revision": "dc3e9a8c8ceccacc41667720fcf79857",
+    "url": "/website/static/media/cable-usb-tipo-c.dc3e9a8c.jpg"
+  },
+  {
+    "revision": "b8d7fe6dfaad54c9912301607bc187cf",
+    "url": "/website/static/media/cargador-blanco.b8d7fe6d.jpg"
+  },
+  {
+    "revision": "ffbeea21cced611b5773ce40b46a1734",
+    "url": "/website/static/media/cargador-rojo.ffbeea21.jpg"
+  },
+  {
+    "revision": "16a742b4e0eded6e1068fa60385c8172",
+    "url": "/website/static/media/cargador-universal.16a742b4.jpg"
+  },
+  {
+    "revision": "4ed9b4de6404eaa339979f67f139fc19",
+    "url": "/website/static/media/mouse.4ed9b4de.jpg"
+  },
+  {
+    "revision": "6a9bfbcc0d57ee080c186c84a448e61a",
+    "url": "/website/static/media/pila-tipo-boton.6a9bfbcc.jpg"
+  },
+  {
+    "revision": "1198b3de863cbd78eaa53d50161a0d13",
+    "url": "/website/static/media/teclado.1198b3de.jpg"
+  },
+  {
+    "revision": "4ebaa1b21c3c9a6abfd1bd4290928a51",
+    "url": "/website/static/media/usb-16gb.4ebaa1b2.jpg"
+  },
+  {
+    "revision": "de285c5bbf3e6557e92a40cdf9056723",
+    "url": "/website/static/media/usb-32gb.de285c5b.jpg"
+  },
+  {
+    "revision": "ff5de5c5f73ee86af20e10ef147725b9",
+    "url": "/website/static/media/usb-8gb.ff5de5c5.jpg"
+  },
+  {
+    "revision": "366fa0620dceda8b3057ef3923cbb4a2",
+    "url": "/website/static/media/diseño-web-caricatura-fondo-oscuro-short.366fa062.jpg"
+  },
+  {
+    "revision": "dd45936b34320ad33de54ea1aabcded3",
+    "url": "/website/static/media/catalogos.dd45936b.jpg"
+  },
+  {
+    "revision": "b312bcbf880379660a601b9253d31981",
+    "url": "/website/static/media/desarrollo-web-adaptativo.b312bcbf.jpg"
+  },
+  {
+    "revision": "c5476e63447681ff6a0cdc7779b3f362",
+    "url": "/website/static/media/edicion-fotos.c5476e63.jpg"
+  },
+  {
+    "revision": "c8c83e1dc5c7d48f2047b8766820eb8e",
+    "url": "/website/static/media/edicion-video.c8c83e1d.jpg"
+  },
+  {
+    "revision": "83522ae2dc10803afa2e4ba7eaf61e6d",
+    "url": "/website/static/media/mujer-emprendedora-pensando.83522ae2.jpg"
+  },
+  {
+    "revision": "c9045a59b88e6b2f7a25a1a8b1caeb9c",
+    "url": "/website/static/media/pad-mouse-personalizado.c9045a59.jpg"
+  },
+  {
+    "revision": "e9664fb4b95448dc928c67f626be292a",
+    "url": "/website/static/media/proceso-reparación-telefono.e9664fb4.jpg"
+  },
+  {
+    "revision": "e25bf380bacf5644c4a0fc40a20ef441",
+    "url": "/website/static/media/redes-sociales.e25bf380.jpg"
+  },
+  {
+    "revision": "160eed090ec289d0acca023633099bcc",
+    "url": "/website/static/media/sublimacion-de-mugs.160eed09.jpg"
+  },
+  {
+    "revision": "b0f6f2f22166636d5c8769116253cc4b",
+    "url": "/website/static/media/tecnico-arreglando-un-pc.b0f6f2f2.jpg"
+  },
+  {
+    "revision": "5dc213d343c165284698f2c4aeb5c57c",
+    "url": "/website/static/media/google-maps-gyp.5dc213d3.jpg"
+  },
+  {
+    "revision": "2fec92fa57579d739488905dcf8242f4",
+    "url": "/website/static/media/icon-instagram.2fec92fa.png"
+  },
+  {
+    "revision": "56b68e6c639d65f4346302ceb3c2e047",
+    "url": "/website/static/media/icon-whatsapp.56b68e6c.png"
+  },
+  {
+    "revision": "409252f90636ea343c24fb3069f5e800",
+    "url": "/website/static/media/back-navbar.409252f9.jpg"
+  },
+  {
+    "revision": "2a359452105e2e04e9c181afd4bd59b6",
+    "url": "/website/static/media/logo-gyp-short.2a359452.png"
+  },
+  {
+    "revision": "b5525a516e9b53d23f5fe06de0c387a9",
+    "url": "/website/index.html"
+  }
+];
